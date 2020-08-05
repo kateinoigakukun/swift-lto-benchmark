3eda1f6b9a1679dec2d816ba37ce07814951427c
Yuta Saito <kateinoigakukun@gmail.com>
2020-08-05 19:08:12 +0900

    Update Example

--- Example/SwiftStdlibExample.swift
+++ Example/SwiftStdlibExample.swift
@@ -1,1 +1,10 @@
-print("Hello")
+@_silgen_name("_runBenchmark")
+public func runBenchmark(_ fn: (Int) -> Void) -> Void
+
+runBenchmark { n in
+    for _ in 0 ..< n {
+        var output = ""
+        print("Hello", to: &output)
+        _ = output
+    }
+}
